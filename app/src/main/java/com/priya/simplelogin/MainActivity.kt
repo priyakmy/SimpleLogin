74dbad25e44be2a3319fbf509531dec3612a830e
Priya singh <priyakum1071@gmail.com>
2023-02-27 23:31:19 +0530

    second commit

--- app/src/main/java/com/priya/simplelogin/MainActivity.kt
+++ app/src/main/java/com/priya/simplelogin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.priya.simplelogin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,31 +10,38 @@ class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MAin", "onCreate: ")
         setContentView(R.layout.activity_main)
     }
 
 
     override fun onStart() {
         super.onStart()
+        Log.d("MAin", "onStart: ")
     }
 
     override fun onRestart() {
         super.onRestart()
+        Log.d("MAin", "onRestart: ")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("MAin", "onResume: ")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("MAin", "onPause: ")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d("MAin", "onStop: ")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("MAin", "onDestroy: ")
     }
 }
