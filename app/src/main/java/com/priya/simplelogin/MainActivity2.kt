913c802108eb6b9f3526c8685aa90d6593e4c11a
Priya singh <priyakum1071@gmail.com>
2023-03-01 15:36:31 +0530

    RelativeLayout

---
+++ app/src/main/java/com/priya/simplelogin/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.priya.simplelogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
